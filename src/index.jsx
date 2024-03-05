9c79229f0b9cbe951074faabceaf3de86513ad44
Yagnesh Modh <contact@thespecialcharacter.com>
2024-03-05 15:44:40 +0530

    commit code

--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,171 @@
-import React from 'react';
+/* eslint-disable max-classes-per-file */
+import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
 import { createRoot } from 'react-dom/client';
-import { Toaster } from '@/components/ui/sonner';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { Toaster } from '@/components/ui/sonner';
+// import { createBrowserRouter } from 'react-router-dom';
 import './style.css';
-import Home from './pages/home';
-import About from './pages/about';
-import DashboardLayout from './layout/dashboardLayout';
-import Products from './pages/products';
-import ProductDetails from './pages/productDetails';
+// import Home from './pages/home';
+// import About from './pages/about';
+// import DashboardLayout from './layout/dashboardLayout';
+// import Products from './pages/products';
+// import ProductDetails from './pages/productDetails';
+// import TickTack from './pages/tickTack';
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <DashboardLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'products',
-        element: <Products />,
-        children: [
-          {
-            path: ':id',
-            element: <ProductDetails />,
-          },
-        ],
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <DashboardLayout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: 'ticktock',
+//         element: <TickTack />,
+//       },
+//       {
+//         path: 'products',
+//         element: <Products />,
+//         children: [
+//           {
+//             path: ':id',
+//             element: <ProductDetails />,
+//           },
+//         ],
+//       },
+//       {
+//         path: 'about',
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ]);
+
+// const HOC = WrappedComponent => {
+//   class HOCComponent extends Component {
+//     state = {
+//       counter: 0,
+//     };
+
+//     increment = () => {
+//       this.setState(({ counter }) => ({
+//         counter: counter + 1,
+//       }));
+//     };
+
+//     render() {
+//       const { counter } = this.state;
+//       return <WrappedComponent counter={counter} increment={this.increment} />;
+//     }
+//   }
+
+//   return HOCComponent;
+// };
+
+// class App extends Component {
+//   render() {
+//     const { counter, increment } = this.props;
+//     return (
+//       <div>
+//         <h1>{counter}</h1>
+//         <button type="button" onClick={increment}>
+//           Increment
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// const WrappedApp = HOC(App);
+
+// class Test extends Component {
+//   render() {
+//     const { counter, increment } = this.props;
+//     return (
+//       <div>
+//         <h1>{counter}</h1>
+//         <button type="button" onClick={increment}>
+//           Increment
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// const WrappedTest = HOC(Test);
+
+// getDerivedStateFromProps
+// GetSnapshotbeforeupdate
+// geDerivedStatefromError
+// componentnDidMout
+
+function App({ number }) {
+  const [counter, setCounter] = useState(() => number * 10);
+  const [name, setName] = useState('');
+  const isMounted = useRef(false);
+
+  const increment = () => {
+    setCounter(val => val + 1);
+  };
+
+  const decrement = () => {
+    setCounter(val => val - 1);
+  };
+
+  // componentDidMount
+
+  // compoentnDidmount
+  // componentDidUpdate
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log('use effect for name');
+    }
+  }, [name]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log('use effect for name and counter');
+    }
+  }, [name, counter]);
+
+  useEffect(() => {
+    console.log('use effect');
+    isMounted.current = true;
+  }, []);
+
+  return (
+    <div>
+      <p> {counter}</p>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+      <button type="button" onClick={decrement}>
+        Decrement
+      </button>
+      <p>{name}</p>
+      <button type="button" onClick={() => setName('yagnesh')}>
+        Change name
+      </button>
+    </div>
+  );
+}
+
+App.propTypes = {
+  number: PropTypes.number.isRequired,
+};
 
 const root = createRoot(document.getElementById('app'));
 root.render(
   <>
-    <RouterProvider router={router} />
+    <App number={5} />
+
+    {/* <WrappedApp /> */}
+    {/* <WrappedTest /> */}
+    {/* <RouterProvider router={router} /> */}
   </>,
 );
---
+++ src/pages/tickTack/index.jsx
@@ -0,0 +1,134 @@
+import { Button } from '@/components/ui/button';
+import React, { PureComponent } from 'react';
+
+export default class TickTack extends PureComponent {
+  state = {
+    isStarted: false,
+    currentPlayer: null,
+    data: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ],
+  };
+
+  render() {
+    const { isStarted, data } = this.state;
+    return (
+      <div>
+        {isStarted ? (
+          <div>
+            <h1>Tick Tack Toe started</h1>
+            <div className="w-80 aspect-square grid grid-rows-3 grid-cols-3 gap-2">
+              {data.map((x, i1) =>
+                x.map((y, i2) => (
+                  <button
+                    type="button"
+                    className="bg-red-200"
+                    key={`${i1}_${i2}`}
+                    onClick={() => {
+                      // winner
+
+                      this.setState(
+                        ({ data, currentPlayer }) => ({
+                          data: [
+                            ...data.slice(0, i1),
+                            [
+                              ...data[i1].slice(0, i2),
+                              currentPlayer === 'O' ? 'O' : 'X',
+                              ...data[i1].slice(i2 + 1),
+                            ],
+                            ...data.slice(i1 + 1),
+                          ],
+                        }),
+                        () => {
+                          const { data, currentPlayer } = this.state;
+
+                          let winner = undefined;
+
+                          for (let i = 0; i < 3; i++) {
+                            if (
+                              (data[i][0] === currentPlayer &&
+                                data[i][1] === currentPlayer &&
+                                data[i][2] === currentPlayer) ||
+                              (data[0][i] === currentPlayer &&
+                                data[1][i] === currentPlayer &&
+                                data[2][i] === currentPlayer)
+                            ) {
+                              winner = currentPlayer;
+                              break;
+                            }
+                          }
+
+                          // Check diagonals
+                          if (
+                            !winner &&
+                            ((data[0][0] === currentPlayer &&
+                              data[1][1] === currentPlayer &&
+                              data[2][2] === currentPlayer) ||
+                              (data[0][2] === currentPlayer &&
+                                data[1][1] === currentPlayer &&
+                                data[2][0] === currentPlayer))
+                          ) {
+                            winner = currentPlayer;
+                          }
+
+                          // Display winner if found
+                          if (winner) {
+                            alert(`Winner is ${winner}`);
+                            return;
+                          }
+
+                          this.setState({
+                            currentPlayer: currentPlayer === 'O' ? 'X' : 'O',
+                          });
+                        },
+                      );
+                    }}
+                  >
+                    {y}
+                  </button>
+                )),
+              )}
+
+              {/* {data.map((x, i) => (
+                <button
+                  type="button"
+                  className="bg-red-200"
+                  key={i}
+                  onClick={() => {
+                    this.setState(
+                      ({ data, currentPlayer }) => ({
+                        data: [
+                          ...data.slice(0, i),
+                          currentPlayer,
+                          ...data.slice(i + 1),
+                        ],
+                        currentPlayer: currentPlayer === 'O' ? 'X' : 'O',
+                      }),
+                      () => {},
+                    );
+                  }}
+                >
+                  {x}
+                </button>
+              ))} */}
+            </div>
+          </div>
+        ) : (
+          <Button
+            onClick={() => {
+              const randomNum = Math.floor(Math.random() * 2);
+              this.setState({
+                isStarted: true,
+                currentPlayer: randomNum === 0 ? 'X' : 'O',
+              });
+            }}
+          >
+            Toss
+          </Button>
+        )}
+      </div>
+    );
+  }
+}
